b2cd6344d7495832d4dcc97f479c6e71c12c4a33
Jakub Dranczewski <domdlapsa@gmail.com>
2018-08-10 11:31:49 +0100

    A more elegtant except in render_fringes Plus some comments

--- magic2/graphics.py
+++ magic2/graphics.py
@@ -4,15 +4,23 @@ from matplotlib.colors import Normalize
 from copy import copy
 
 
+# This function can be used to draw the fringes on a given canvas
+# at a specified line width. 'fringes' is a Fringes object.
 def render_fringes(fringes, canvas, width=1):
+    # Hovewer nasty this nested loop looks, it allows us to easily iterate
+    # over all points in all fringes. The x range and y range are also
+    # executed only once if width is 0
     for fringe in fringes.list:
         for point in fringe.points:
             colour = 1
             for x in range(-width+1, width):
                 for y in range(-width+1, width):
+                    # This try...excpt saves us from problems with running out
+                    # of canvas space while alerting us of other valid errors
+                    # that could occur
                     try:
                         canvas[point[0]+y, point[1]+x] = colour
-                    except:
+                    except IndexError:
                         pass
 
 # Define a normalization and a colour map that can be used with
