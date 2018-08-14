f302784f52fb5986ab5e603d0cd701c371f99ef3
Jakub Dranczewski <domdlapsa@gmail.com>
2018-08-14 11:09:55 +0100

    Detection of convex and concave triangles Instead of convex hulls this method works with areas - if we flip the joining edge, the area should not change. If it does, the triangles form a concave quadrilateral. This also allows us to check whether one of the triangles doesn't have a 0 area.

--- magic2/triangulate.py
+++ magic2/triangulate.py
@@ -43,21 +43,53 @@ class Triangulation:
         changes = 1
         while changes:
             changes = 0
-            for i in range(len(self.flat_triangles)):
-                triangle = self.triangles[i]
-                neighbour = triangle.get_sloped_neighbour(self)
+            i = 0
+            while i < len(self.flat_triangles):
+                triangle = self.triangles[self.flat_triangles[i]]
+                neighbour, op1, op2 = triangle.get_sloped_neighbour(self)
                 if neighbour is None:
-                    continue
-                # ch = len(ConvexHull(points[triangle.vertices]).vertices)
-                all_points = sp.concatenate(
-                    (self.points[triangle.vertices, :],
-                     self.points[neighbour.vertices, :]), 0)
-                # ch = len(ConvexHull(all_points).vertices)
-                ch = 4
-                if ch == 4:
-                    pass
-                elif ch == 3:
-                    pass
+                    print("none")
+                    # del self.flat_triangles[i]
+                    i += 1
+                else:
+                    # all_points = sp.concatenate(
+                    #     (self.points[triangle.vertices, :],
+                    #      self.points[neighbour.vertices, :]), 0)
+                    # ch = len(ConvexHull(all_points).vertices)
+                    ch = 4
+                    ai1 = 0.5 * abs(
+                          (triangle.vert_coordinates[op1, 1] - triangle.vert_coordinates[(op1+2)%3, 1])
+                        * (triangle.vert_coordinates[(op1+1)%3, 0] - triangle.vert_coordinates[op1, 0])
+                        - (triangle.vert_coordinates[op1, 1] - triangle.vert_coordinates[(op1+1)%3, 1])
+                        * (triangle.vert_coordinates[(op1+2)%3, 0] - triangle.vert_coordinates[op1, 0])
+                    )
+                    ai2 = 0.5 * abs(
+                          (neighbour.vert_coordinates[op2, 1] - neighbour.vert_coordinates[(op2+2)%3, 1])
+                        * (neighbour.vert_coordinates[(op2+1)%3, 0] - neighbour.vert_coordinates[op2, 0])
+                        - (neighbour.vert_coordinates[op2, 1] - neighbour.vert_coordinates[(op2+1)%3, 1])
+                        * (neighbour.vert_coordinates[(op2+2)%3, 0] - neighbour.vert_coordinates[op2, 0])
+                    )
+                    af1 = 0.5 * abs(
+                          (triangle.vert_coordinates[op1, 1] - triangle.vert_coordinates[(op1+2)%3, 1])
+                        * (neighbour.vert_coordinates[op2, 0] - triangle.vert_coordinates[op1, 0])
+                        - (triangle.vert_coordinates[op1, 1] - neighbour.vert_coordinates[op2, 1])
+                        * (triangle.vert_coordinates[(op1+2)%3, 0] - triangle.vert_coordinates[op1, 0])
+                    )
+                    af2 = 0.5 * abs(
+                          (triangle.vert_coordinates[op1, 1] - triangle.vert_coordinates[(op1+1)%3, 1])
+                        * (neighbour.vert_coordinates[op2, 0] - triangle.vert_coordinates[op1, 0])
+                        - (triangle.vert_coordinates[op1, 1] - neighbour.vert_coordinates[op2, 1])
+                        * (triangle.vert_coordinates[(op1+1)%3, 0] - triangle.vert_coordinates[op1, 0])
+                    )
+                    # print(ai1, ai2, af1, af2)
+                    if ai1 + ai2 == af1 + af2:
+                        print("convex")
+                        # del self.flat_triangles[i]
+                        i += 1
+                    else:
+                        # print("concave")
+                        # del self.flat_triangles[i]
+                        i += 1
 
 
 # Calculate the distance between two points in the points list
@@ -71,7 +103,9 @@ class Triangle:
     def __init__(self, dt, index, points, values):
         # Copy the vertex and neighbours info
         self.vertices = dt.simplices[index]
+        self.vert_coordinates = points[self.vertices]
         self.neighbours = dt.neighbors[index]
+        self.index = index
         # Check whether the triangle is flat
         self.flat = (values[self.vertices[0]] == values[self.vertices[1]]
                      and values[self.vertices[1]] == values[self.vertices[2]])
@@ -99,11 +133,12 @@ class Triangle:
             # If the edge is long
             if self.long_edges[i]:
                 n_index = self.neighbours[i]
-                # If the neighbour exists and is flat, return its index
+                # If the neighbour exists and isn't flat, return its index
                 if n_index != -1 and not tri.triangles[n_index].flat:
-                    return tri.triangles[n_index]
+                    neighbour = tri.triangles[n_index]
+                    return neighbour, i, sp.argwhere(neighbour.neighbours == self.index)
         # If no neighbour found, return None
-        return None
+        return None, None, None
 
 
 def triangulate(canvas):
@@ -117,3 +152,8 @@ def triangulate(canvas):
     print("Optimisation")
     tri.optimise()
     print("Finished")
+    plt.imshow(canvas.fringe_phases, cmap=m2graphics.cmap)
+    plt.triplot(tri.points[:, 1], tri.points[:, 0], tri.get_simplices())
+    print(tri.flat_triangles)
+    plt.triplot(tri.points[:, 1], tri.points[:, 0], [tri.triangles[i].vertices for i in tri.flat_triangles])
+    plt.show()
