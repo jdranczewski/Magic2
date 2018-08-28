f109f21d9f3fc019c5f0ce0de4030d2bd1501f00
Jakub Dranczewski <domdlapsa@gmail.com>
2018-08-28 16:35:40 +0100

    Convert the bat to exe
    This uses http://www.f2ko.de/en/b2e.php, giving a console-less mode of execution.

--- Magic2.bat
+++
@@ -1,2 +0,0 @@
-python main.py
-if ERRORLEVEL 1 "%UserProfile%\Anaconda3\python" main.py
---
+++ bat.bat
@@ -0,0 +1,35 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFC5HSRa+GGS5E7gZ5vzo08eGpUQeBt4yeYjS2ebAKeMcig==
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSTk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVsFAlTi
+::ZQ05rAF9IAHYFVzEqQIdKRxdT1bi
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATE3kM7PRkUaBaDMmigA6IP4+Gb
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFC5HSRa+GGS5E7gZ5vzo08eGpUQeBt4SeYjS2ebAJfgWig==
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+python main.py
+if ERRORLEVEL 1 "%UserProfile%\Anaconda3\python" main.py
